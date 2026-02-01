76d9ebf5f4551cf3c3a913e38958dcf23f11a655
Adicyber2 <adityadeshmukh1404@gmail.com>
2026-02-01 13:35:44 +0530

    frontend added

--- day-8/backend/src/app.js
+++ day-8/backend/src/app.js
@@ -1,8 +1,12 @@
 const express=require("express")
 const noteModel=require("./models/notes-model")
 
+const cors=require("cors")
+
 const app = express()
+app.use(cors())
 app.use(express.json())
+
 
 app.post('/api/notes',async (req,res)=>{
     const{title,description}=req.body
